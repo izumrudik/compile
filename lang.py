dd6ed202776f12126bdfc6ae9b1d4d51a30e0652
s <fedor.tryfanau@gmail.com>
2022-01-26 23:28:00 +0300

    added implementation for lex() function

--- lang.py
+++ lang.py
@@ -1,9 +1,10 @@
 #!/bin/python3.10
 from dataclasses import dataclass
-from email.policy import default
+from enum import Enum, auto
+from functools import lru_cache as cache
 from sys import argv, stderr
 
-@dataclass()
+@dataclass
 class Config:
 	self_name:str
 	file:str|None = None
@@ -55,11 +56,126 @@ def extract_file_text_from_config(config:Config) -> str:
 		exit(3)
 	with open(config.file,'r') as file:
 		text = file.read()
-	print(text)
-	return text
+	return text+' '*10
+@cache
+@dataclass
+class Loc:
+	file_path:str
+	file_text:str
+	idx:int = 0
+	rows:int = 1
+	cols:int = 1
 
-def lex(text,config):
-	assert False, " 'lex' is not implemented yet"
+	def __add__(self,n):
+		idx,cols,rows = self.idx,self.cols,self.rows
+		for _ in range(n):
+			idx+=1
+			cols+=1
+			if self.file_text[idx] =='\n':
+				cols = 0
+				rows+= 1
+		return type(self)(self.file_path,self.file_text,idx,rows,cols)
+	
+	def __repr__(self):
+		return f"{self.file_path}:{self.rows}:{self.cols}"
+
+	def __lt__(self,idx):
+		return self.idx < idx
+
+	@property
+	def char(self):
+		return self.file_text[self.idx]
+	
+	def __bool__(self):
+		return self.idx < len(self.file_text)-1
+
+
+WHITESPACE    = " \t\n\r\v\f"
+WORD_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DIGITS        = "0123456789"
+class Token_type(Enum):
+	digit               = auto()
+	word                = auto()
+	left_curly_bracket  = auto()
+	right_curly_bracket = auto()
+	left_parenthesis    = auto()
+	right_parenthesis   = auto()
+	string              = auto()
+@dataclass
+class Token:
+	loc:Loc
+	typ:Token_type
+	operand: str | None = None
+	
+	def __repr__(self) -> str:
+		return f"{self.loc}\t{self.typ}({self.operand})"
+
+def lex(text:str,config:Config):
+	loc=Loc(config.file,text,)
+	programm:list[Token] = []
+	while loc:
+		s = loc.char
+		start_loc = loc
+		if s in WHITESPACE:
+			loc+=1
+			continue
+
+		elif s in DIGITS:
+			word = s
+			loc+=1
+			while loc.char in DIGITS:
+				word+=loc.char
+				loc+=1
+			programm.append(Token(start_loc,Token_type.digit,word))
+			continue
+
+		elif s in WORD_ALPHABET:
+			word = s
+			loc+=1
+
+			while loc.char in WORD_ALPHABET:
+				word+=loc.char
+				loc+=1
+			
+			programm.append(Token(start_loc,Token_type.word,word))
+			continue
+			
+		elif s in "'\"":
+			loc+=1
+			word = ''
+			while loc.char != s:
+				if loc.char == '\\':
+					loc+=1
+					word+={
+						'n':'\n',
+						't':'\t',
+						'v':'\v',
+						'r':'\r',
+						}.get(loc.char,loc.char)
+					continue
+				word+=loc.char
+				loc+=1
+			programm.append(Token(start_loc,Token_type.string,word))
+			
+		elif s in '}{()':
+			programm.append(Token(start_loc,
+			{
+				'{':Token_type.left_curly_bracket,
+				'}':Token_type.right_curly_bracket,
+				'(':Token_type.left_parenthesis,
+				')':Token_type.right_parenthesis,
+			}[s]))
+
+		elif s == '#':
+			while loc.char != '\n':
+				loc+=1
+			continue
+
+		else:
+			print(f"ERROR: {loc}: Illigal char '{s}'")
+			exit(4)
+		loc+=1
+	for i in programm:print(i)
 
 def parse(words,config):
 	assert False, " 'parse' is not implemented yet"
@@ -79,8 +195,8 @@ def run_assembler(config):
 def main():
 	config = process_cmd_args(argv)
 	text = extract_file_text_from_config(config)
-	exit(0) 
 	words = lex(text,config)
+	exit(0) 
 	ast = parse(words,config)
 	type_check(ast,config)
 	compile_to_assembly(ast,config)
