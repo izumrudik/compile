53ba8b23201a3b81548d9438e071126dc6699e6a
s <fedor.tryfanau@gmail.com>
2022-01-26 20:33:00 +0300

    implemented process_cmd_args function in a way that would be easy to extend in future

--- lang.py
+++ lang.py
@@ -1,9 +1,46 @@
-#!/bin/python3
-from sys import argv
+#!/bin/python3.10
+from dataclasses import dataclass
+from email.policy import default
+from sys import argv, stderr
+from typing import Optional
 
+@dataclass()
+class Config:
+	self_name:str
+	file:Optional[str] = None
+def usage(self_name):
+	return f"""Usage:
+	{self_name} file [flags]
+Flags:
+	Not any yet
+"""
 
 def process_cmd_args(args):
-	assert False, " 'process_cmd_args' is not implemented yet"
+	self_name = args[0]
+	config = Config(self_name)
+	args = args[1:]
+	for arg in args:
+		match arg[0],arg[1],arg[2:]:
+			case '-','-',flag:
+				print(f"ERROR: flag {flag} is not supported yet",file=stderr)
+				print(usage(self_name))
+				exit(1)
+			case '-',flag,rest:
+				for subflag in flag+rest:#-smth
+					match subflag:
+						case wildcard:
+							print(f"ERROR: flag -{wildcard} is not supported yet",file=stderr)
+							print(usage(self_name))
+							exit(2)	
+			case file,rest1,rest2:
+				if config.file is not None:
+					print(f"ERROR: provided 2 files",file=stderr)
+					print(usage(self_name))
+					exit(3)
+				file+=rest1+rest2
+				config.file = file				
+	print(config)
+	return config
 
 def extract_file_text_from_config(config):
 	assert False, " 'extract_file_text_from_config' is not implemented yet"
