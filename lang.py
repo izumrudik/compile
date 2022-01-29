48657df3eec8caa1489a355cd447fe984a4be306
s <fedor.tryfanau@gmail.com>
2022-01-29 18:57:04 +0300

    parsing done

--- lang.py
+++ lang.py
@@ -1,13 +1,13 @@
 #!/bin/python3.10
-from dataclasses import dataclass
+from dataclasses import dataclass,field
 from enum import Enum, auto
-from functools import lru_cache as cache
 from sys import argv, stderr
-#exit>9
+from typing import Callable
 @dataclass
 class Config:
 	self_name:str
 	file:str|None = None
+	compile_time_rules:list[tuple['Loc',str]] = field(default_factory=list)
 
 def usage(config:Config) -> str:
 	return f"""Usage:
@@ -57,7 +57,7 @@ def extract_file_text_from_config(config:Config) -> str:
 	with open(config.file,'r') as file:
 		text = file.read()
 	return text+' '*10
-@cache
+
 @dataclass
 class Loc:
 	file_path:str
@@ -69,7 +69,7 @@ class Loc:
 	def __add__(self,n):
 		idx,cols,rows = self.idx,self.cols,self.rows
 		if idx+n>=len(self.file_text):
-			print(f"ERROR: {self}: unexpected end of file")
+			print(f"ERROR: {self}: unexpected end of file",file=stderr)
 			exit(5)
 		for _ in range(n):
 			idx+=1
@@ -111,6 +111,16 @@ class TT(Enum):
 	string              = auto()
 	EOF                 = auto()
 	arrow               = auto()
+	semicolon           = auto()
+	comma               = auto()
+	equals_sign         = auto()
+	plus                = auto()
+	minus               = auto()
+	asterisk            = auto()
+	double_asterisk     = auto()
+	slash               = auto()
+	double_slash        = auto()
+	percent_sign        = auto()
 @dataclass
 class Token:
 	loc:Loc
@@ -118,7 +128,9 @@ class Token:
 	operand: str | None = None
 	
 	def __repr__(self) -> str:
-		return f"{self.loc}\t{self.typ}({self.operand})"
+		if self.operand is not None:
+			return f"{self.operand}"
+		return f"{self.typ}"
 	
 	def eq(self,typ:TT,operand: str | None = None) -> bool:
 		return self.typ == typ and self.operand == self.operand
@@ -172,37 +184,105 @@ def lex(text:str,config:Config) -> list[Token]:
 				loc+=1
 			programm.append(Token(start_loc,TT.string,word))
 			
-		elif s in '}{()':
+		elif s in '}{(),;=+-%':
 			programm.append(Token(start_loc,
 			{
 				'{':TT.left_curly_bracket,
 				'}':TT.right_curly_bracket,
 				'(':TT.left_parenthesis,
 				')':TT.right_parenthesis,
+				';':TT.semicolon,
+				'=':TT.equals_sign,
+				'+':TT.plus,
+				'-':TT.minus,
+				'%':TT.percent_sign,
+				',':TT.comma,
 			}[s]))
+
+
+		elif s == '*':
+			token = Token(start_loc,TT.asterisk)
+			loc+=1
+			if loc.char == '*':
+				loc+=1
+				token = Token(start_loc,TT.double_asterisk)
+			programm.append(token)
+			continue
+
+		elif s == '/':
+			token = Token(start_loc,TT.slash)
+			loc+=1
+			if loc.char == '/':
+				token = Token(start_loc,TT.double_slash)
+				loc+=1
+			programm.append(token)
+			continue
 
 		elif s == '-':
 			loc+=1
 			if loc.char == '>':
 				programm.append(Token(start_loc,TT.arrow))
 			else:
-				print(f"ERROR: {loc}: unrecognized sequence: '{s}{loc.char}'")
+				print(f"ERROR: {loc}: unrecognized sequence: '{s}{loc.char}'",file=stderr)
 				exit(9)
 
 		elif s == '#':
 			while loc.char != '\n':
 				loc+=1
 			continue
-
+		elif s == '!':
+			word = ''
+			while loc.char != '\n':
+				word+=loc.char
+				loc+=1
+			config.compile_time_rules.append((start_loc,word))
+			continue
 		else:
-			print(f"ERROR: {loc}: Illigal char '{s}'")
+			print(f"ERROR: {loc}: Illigal char '{s}'",file=stderr)
 			exit(6)
 		loc+=1
+	programm.append(Token(start_loc,TT.EOF))
 	return programm
+class Node:...
 
+def join(listik,sep=','):
+	return sep.join([repr(i) for i in listik])
+@dataclass
+class Node_tops(Node):
+	tops:list[Node]
+	def __repr__(self) -> str:
+		newline = '\n'
+		return f"{join(self.tops,newline)}"
+@dataclass
+class Node_function_call(Node):
+	name:Token
+	args:list[Node|Token]
+	def __repr__(self) -> str:
+		return f"{self.name}({join(self.args)})"
+@dataclass
+class Node_binary_expression(Node):
+	left:Token | Node
+	op:Token
+	right:Token | Node
+	def __repr__(self) -> str:
+		return f"{self.left} {self.op} {self.right}"
+@dataclass
+class Node_fun(Node):
+	name:Token
+	input_types:list[Token]
+	output_types:list[Token]
+	code:"Node_code"
+	def __repr__(self) -> str:
+		return f"fun {self.name} {join(self.input_types)}->{join(self.output_types)} {self.code}"
 
+@dataclass
+class Node_code(Node):
+	statements:list[Node]
 
-
+	def __repr__(self) -> str:
+		nl = '\n'
+		tab = '\t'
+		return f"{'{'}{nl}{tab}{join(self.statements,f';{nl}{tab}')}{nl}{'}'}"
 
 class Parser:
 	def __init__(self,words:list[Token],config:Config) -> None:
@@ -221,16 +301,17 @@ class Parser:
 	def parse(self):
 		nodes = []
 		while self.current.typ != TT.EOF:
-			nodes.append(self.parse_lvl0())
-		return nodes
+			nodes.append(self.parse_top())
+		return Node_tops(nodes)
 
-	def parse_lvl0(self):
+	def parse_top(self):
 		if self.current.eq(TT.keyword,'fun'):
 			self.adv()
 			if self.current.typ != TT.word:
-				print(f"ERROR: {self.current.loc}: expected name of function after keyword 'fun'")
+				print(f"ERROR: {self.current.loc}: expected name of function after keyword 'fun'",file=stderr)
 				exit(8)
 			name = self.current
+			loc = self.current.loc
 			self.adv()
 			#parse contract of the fun
 			input_types = [] 
@@ -243,15 +324,114 @@ class Parser:
 				while self.current.typ == TT.word:
 					raise NotImplementedError()
 
-			if self.current.typ == TT.left_curly_bracket:
-				...
 
-			
+			code = self.parse_code_block()
+
+			return Node_fun(name,input_types,output_types,code)
 		else:
-			print(f"ERROR: {self.current.loc}: unrecognized top-level structure while parsing")
+			print(f"ERROR: {self.current.loc}: unrecognized top-level structure while parsing",file=stderr)
 			exit(7)
 	
+	def parse_code_block(self):
+		if self.current.typ != TT.left_curly_bracket:
+			print(f"ERROR: {self.current.loc}: expected code block starting with '{'{'}' ",file=stderr)
+			exit(10)
+		self.adv()
+		code=[]
+		while self.current.typ != TT.right_curly_bracket:
+			code.append(self.parse_statement()) 
+			if self.current.typ == TT.right_curly_bracket:break
+			if self.current.typ != TT.semicolon:
+				print(f"ERROR: {self.current.loc}: expected ';' or '{'}'}' ",file=stderr)
+				exit(11)
+			self.adv()
+		self.adv()
+		return Node_code(code)	
+		
+	@property
+	def next(self):
+		if len(self.words)>self.idx+1:
+			return self.words[self.idx+1]
 
+	def parse_statement(self):
+		if self.next is not None:
+			if self.next.typ == TT.equals_sign:
+				raise NotImplementedError()
+		
+		return self.parse_expression()
+		
+
+	def parse_expression(self):
+		return self.parse_exp0()
+
+	def bin_exp_parse_helper(self,nexp:Callable[[],Node],operations:tuple[TT]) -> Node:
+		left = nexp()
+		while self.current.typ in operations:
+			op_token = self.current
+			self.adv()
+			right = nexp()
+
+			left = Node_binary_expression(left,op_token,right)
+
+		return left
+
+	def parse_exp0(self):
+		nexp = self.parse_exp1
+		return self.bin_exp_parse_helper(nexp,[
+			TT.plus,
+			TT.minus,
+		])
+		
+
+	def parse_exp1(self):
+		nexp = self.parse_exp2
+		return self.bin_exp_parse_helper(nexp,[
+			TT.asterisk,
+			TT.slash,
+		])
+	
+	def parse_exp2(self):
+		nexp = self.parse_term
+		return self.bin_exp_parse_helper(nexp,[
+			TT.double_asterisk,
+			TT.double_slash,
+			TT.percent_sign,
+		])		
+	
+	def parse_term(self):
+		if self.current.typ in (TT.digit,TT.string):
+			token = self.current
+			self.adv()
+			return token
+		if self.current.typ == TT.left_parenthesis:
+			self.adv()
+			expr = self.parse_expression()
+			if self.current.typ != TT.right_parenthesis:
+				print(f"ERROR: {self.current.loc}: expected '('",file=stderr)
+				exit(12)
+			self.adv()
+			return expr
+		if self.current.typ == TT.word: #trying to extract function call
+			name = self.current
+			self.adv()
+			if self.current.typ != TT.left_parenthesis:
+				print(f"ERROR: {self.current.loc}: later calling is not allowed yet",file=stderr)
+				exit(1000)
+				return name #to call later 
+			self.adv()
+			args = []
+			while self.current.typ != TT.right_curly_bracket:
+				args.append(self.parse_expression()) 
+				if self.current.typ == TT.right_parenthesis:break
+				if self.current.typ != TT.comma:
+					print(f"ERROR: {self.current.loc}: expected ',' or ')' ",file=stderr)
+					exit(13)
+				self.adv()
+			self.adv()
+			return Node_function_call(name,args)
+			
+	
+	
 
 def type_check(ast,config):
 	assert False, " 'type_check' is not implemented yet"
@@ -269,7 +449,6 @@ def main():
 	config = process_cmd_args(argv)
 	text = extract_file_text_from_config(config)
 	tokens = lex(text,config)
-	for token in tokens: print(token)
 	ast = Parser(tokens,config).parse()
 	print(ast)
 	exit(0)
