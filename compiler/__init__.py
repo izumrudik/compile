64165524411feca4ada0d172c244191348a22282
s <fedor.tryfanau@gmail.com>
2022-02-25 22:12:55 +0300

    prepared for diffrent flavours

--- compiler/__init__.py
+++ compiler/__init__.py
@@ -4,8 +4,7 @@ from .lexer import lex
 from .primitives import process_cmd_args, run_assembler, run_command
 from .parser import Parser
 from .type_checker import TypeCheck
-from .generator import GenerateAssembly
-from .utils import  extract_ast_from_file_name, dump_ast, dump_tokens
+from .utils import  extract_ast_from_file_name, dump_ast, dump_tokens, generate_assembly
 def main() -> None:
 	config = process_cmd_args(argv)#["me", "foo.lang"])
 	tokens, ast = extract_ast_from_file_name(config.file,config)
@@ -13,8 +12,8 @@ def main() -> None:
 	dump_ast(ast, config)
 
 	TypeCheck(ast, config)
-
-	GenerateAssembly(ast, config)
+	
+	generate_assembly(ast, config)
 	run_assembler(config)
 	if config.run_file and config.run_assembler:
 		ret_code = run_command([f"./{config.output_file}.out"], config)
--- compiler/utils.py
+++ compiler/utils.py
@@ -3,6 +3,13 @@ import sys
 from .primitives import nodes, Token, Config, extract_file_text_from_file_name
 from . import lexer
 from . import parser
+from . import x86_64_nasm_generator
+__all__ = [
+	"dump_tokens",
+	"dump_ast",
+	"extract_ast_from_file_name",
+	"generate_assembly",
+]
 def dump_tokens(tokens:'list[Token]', config:Config) -> None:
 	if not config.dump:
 		return
@@ -22,3 +29,6 @@ def extract_ast_from_file_name(file_name:str, config:Config) -> 'tuple[list[Toke
 
 	ast:nodes.Tops = parser.Parser(tokens, config).parse()
 	return tokens, ast
+
+def generate_assembly(ast:'nodes.Tops', config:Config) -> None:
+	x86_64_nasm_generator.GenerateAssembly(ast,config)# no flavours for now
